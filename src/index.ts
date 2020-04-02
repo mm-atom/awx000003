93d1fb277e0170c871d61dba855bb94ee596a70e
taoqf <tao_qiufeng@126.com>
2020-04-02 17:34:02 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
+import global from '@mmstudio/global';
 
-export default function awx3(param1: string, param2: string) {
+/**
+ * 调用服务
+ * @param service_name 服务类型
+ * @param msg 参数
+ */
+export default async function nodejs<T extends object>(service_name: string, msg: unknown) {
+	const url = `${global('host', 'http://127.0.0.1:8889')}/sendmessage`;
+	const data = JSON.stringify({
+		msg,
+		type: service_name
+	});
+	return post<T>(url, data);
+}
+
+function post<T extends object>(url: string, data: string | object | ArrayBuffer, headers?: { [key: string]: string; }) {
+	const header = {
+		cookie: wx.getStorageSync('cookie'),
+		...headers
+	};
+	return new Promise<T>((resolve, reject) => {
+		wx.request({
+			data,
+			header,
+			method: 'POST',
+			url,
+			success(res) {
+				const cookie = res.header['Set-Cookie'];
+				if (cookie) {
+					wx.setStorageSync('cookie', res.header['Set-Cookie']);
+				}
+				if (res.statusCode > 0 && res.statusCode < 400) {
+					resolve(res.data as T);
+				} else {
+					reject(res);
+				}
+			},
+			fail(res) {
+				reject({ data: { msg: res.errMsg, detail: res.errMsg } });
+			}
+		});
+	});
 }
